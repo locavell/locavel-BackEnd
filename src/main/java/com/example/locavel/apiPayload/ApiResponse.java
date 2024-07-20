8febb4ebecbd24c5ee8d66a5e5d20b1000eda871
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-20 17:15:03 +0900

    feat: ApiResponse 코드 작성

---
+++ src/main/java/com/example/locavel/apiPayload/ApiResponse.java
@@ -0,0 +1,38 @@
+package com.example.locavel.apiPayload;
+
+import com.example.locavel.apiPayload.code.BaseCode;
+import com.example.locavel.apiPayload.code.status.SuccessStatus;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@JsonPropertyOrder({"isSuccess", "code", "message", "result"})
+public class ApiResponse<T> {
+
+    @JsonProperty("isSuccess")
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private T result;   //generic type: 어떤 값이 올지 모르기 때문에 generic 으로 선언, 추후에 외부에서 지정 가능
+
+
+    // 성공한 경우 응답 생성
+
+    public static <T> ApiResponse<T> onSuccess(T result){
+        return new ApiResponse<>(true, SuccessStatus._OK.getCode() , SuccessStatus._OK.getMessage(), result);
+    }
+
+    public static <T> ApiResponse<T> of(BaseCode code, T result){
+        return new ApiResponse<>(true, code.getReasonHttpStatus().getCode() , code.getReasonHttpStatus().getMessage(), result);
+    }
+
+    // 실패한 경우 응답 생성
+    public static <T> ApiResponse<T> onFailure(String code, String message, T data){
+        return new ApiResponse<>(false, code, message, data);
+    }
+}
