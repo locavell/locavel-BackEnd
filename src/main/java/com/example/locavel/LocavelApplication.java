3ef615a925742ed1199ac675e336711a6ba73057
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-20 16:34:54 +0900

    first init

---
+++ src/main/java/com/example/locavel/LocavelApplication.java
@@ -0,0 +1,13 @@
+package com.example.locavel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocavelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LocavelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/locavel/LocavelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.locavel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocavelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
