ec173fd79b9e83c2efff02b8dc7152af2e323a3b
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-20 21:21:32 +0900

    Feat: Places Entity 생성 및 Mapping

---
+++ src/main/java/com/example/locavel/domain/Places.java
@@ -0,0 +1,41 @@
+package com.example.locavel.domain;
+
+import com.example.locavel.domain.common.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Places extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)//지연 로딩
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToOne
+    @JoinColumn(name = "region_id")
+    private Region region;
+
+    private String name;
+
+    private String description;
+
+    private String spot;
+
+    private String food;
+
+    private String activity;
+
+    private LocalDateTime createdAt;
+
+}
