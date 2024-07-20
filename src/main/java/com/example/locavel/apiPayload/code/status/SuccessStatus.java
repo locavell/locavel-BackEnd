1f66ffb0b5ec486d2f51b940eec901862b368fed
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-20 17:16:57 +0900

    feat: SuccessStatus Enum 클래스 생성

---
+++ src/main/java/com/example/locavel/apiPayload/code/status/SuccessStatus.java
@@ -0,0 +1,40 @@
+package com.example.locavel.apiPayload.code.status;
+
+import com.example.locavel.apiPayload.code.BaseCode;
+import com.example.locavel.apiPayload.code.ReasonDTO;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@AllArgsConstructor
+public enum SuccessStatus implements BaseCode {
+
+    //일반적인 응답
+    _OK(HttpStatus.OK, "COMMON200", "성공입니다.");
+
+    private final HttpStatus httpStatus;
+    private final String code;
+    private final String message;
+
+    @Override
+    public ReasonDTO getReason() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .build();
+    }
+
+    @Override
+    public ReasonDTO getReasonHttpStatus() {
+        return ReasonDTO.builder()
+                .message(message)
+                .code(code)
+                .isSuccess(true)
+                .httpStatus(httpStatus)
+                .build()
+                ;
+    }
+
+}
