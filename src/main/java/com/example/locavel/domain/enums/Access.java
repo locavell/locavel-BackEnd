b05114e3370305719e0d4cb2e09b11cb075173c9
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-24 15:19:44 +0900

    Fix: Spring Security 권한 접두사 오류 수정

--- src/main/java/com/example/locavel/domain/enums/Access.java
+++ src/main/java/com/example/locavel/domain/enums/Access.java
@@ -1,6 +1,13 @@
 package com.example.locavel.domain.enums;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum Access {
-    GUEST,//첫 로그인 시 GUEST
-    USER//추가 정보 기입시 USER
+    GUEST("ROLE_GUEST"),//첫 로그인 시 GUEST
+    USER("ROLE_USER");//추가 정보 기입시 USER
+
+    private final String key;
 }
