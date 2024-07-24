2f4a1eeaef0944766f4a4f48ec92cc13b9b48551
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-24 15:26:39 +0900

    Feat: UserRepository 생성

---
+++ src/main/java/com/example/locavel/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.example.locavel.repository;
+
+import com.example.locavel.domain.User;
+import com.example.locavel.domain.enums.SocialType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByNickname(String nickname);
+
+    Optional<User> findBySocialTypeAndSocialId(SocialType socialType, String socialId);
+
+    Optional<User> findByRefreshToken(String refreshToken);
+}
