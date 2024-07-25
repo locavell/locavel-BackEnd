c4d2a4a69b406e795fc685dd3a70c21cf8ec0a86
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-25 13:47:58 +0900

    Feat: UserController 생성

---
+++ src/main/java/com/example/locavel/web/UserController.java
@@ -0,0 +1,27 @@
+package com.example.locavel.web;
+
+import com.example.locavel.service.UserCommandService;
+import com.example.locavel.web.dto.UserDTO.UserSignUpDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserCommandService userCommandService;
+
+    @PostMapping("/api/sign-up")
+    public String signUp(@RequestBody UserSignUpDto userSignUpDto) throws Exception{
+        userCommandService.signUp(userSignUpDto);
+        return "sign-up Success";
+    }
+
+    @GetMapping("/jwt-test")//테스트용
+    public String jwtTest(){
+        return "jwt-Test Success";
+    }
+}
