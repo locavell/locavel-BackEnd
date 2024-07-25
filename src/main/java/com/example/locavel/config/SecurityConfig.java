2f1646ef13f1749fea95d7893a3d733b79cd0617
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-25 14:20:04 +0900

    Feat: SecurityConfig 임시 추가

---
+++ src/main/java/com/example/locavel/config/SecurityConfig.java
@@ -0,0 +1,25 @@
+package com.example.locavel.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeRequests(authz -> authz
+                        .requestMatchers("/api/sign-up").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
+    }
+}
+
