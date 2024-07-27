e943fbe3941b3fc33f3f8a5cc9fe535b8aef2482
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-07-27 13:17:14 +0900

    Feat: sendAccessToken 메서드 생성

--- src/main/java/com/example/locavel/service/jwtService/JwtService.java
+++ src/main/java/com/example/locavel/service/jwtService/JwtService.java
@@ -3,6 +3,7 @@ package com.example.locavel.service.jwtService;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.example.locavel.repository.UserRepository;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -66,4 +67,16 @@ public class JwtService {
                 .withExpiresAt(new Date(now.getTime() + refreshTokenExpirationPeriod))//리프레시 토큰 만료 시간 설정
                 .sign(Algorithm.HMAC512(secretKey));
     }
+
+    /**
+     * AccessToken 헤더에 담아서 보내기
+     * */
+    public void sendAccessToken(HttpServletResponse response, String accessToken){
+        response.setStatus(HttpServletResponse.SC_OK);//200 상태 코드
+        response.setHeader(accessHeader, accessToken);
+        log.info("재발급된 AccessToken : {}", accessToken);//액세스 토큰 확인
+    }
+    /**
+     * AccessToken + RefreshToken 헤더에 담아서 보내기
+     * */
 }
