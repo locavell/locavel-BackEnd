395a4bb54db3a744547c6e99c1e3064f7c75a24d
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-08-01 17:59:11 +0900

    Feat: OAuth2LoginSuccessHandler 작성

---
+++ src/main/java/com/example/locavel/handler/OAuth2LoginSuccessHandler.java
@@ -0,0 +1,55 @@
+package com.example.locavel.handler;
+
+import com.example.locavel.domain.enums.Access;
+import com.example.locavel.domain.oauth.CustomOAuth2User;
+import com.example.locavel.repository.UserRepository;
+import com.example.locavel.service.jwtService.JwtService;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class OAuth2LoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtService jwtService;
+    private final UserRepository userRepository;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        log.info("OAuth 2 Login 성공");
+        try{
+            CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
+
+            if(oAuth2User.getAccess() == Access.GUEST){
+                String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
+                response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
+                response.sendRedirect("oauth2/sign-up");// 프론트의 회원가입 추가 정보 입력 폼으로 리다이렉트
+
+                jwtService.sendAccessAndRefreshToken(response, accessToken, null);
+            } else {
+                loginSuccess(response, oAuth2User);
+            }
+        }catch (Exception e){
+            throw e;
+        }
+    }
+
+    private void loginSuccess(HttpServletResponse response, CustomOAuth2User oAuth2User) throws IOException {
+        String accessToken = jwtService.createAccessToken(oAuth2User.getEmail());
+        String refreshToken = jwtService.createRefreshToken();
+        response.addHeader(jwtService.getAccessHeader(), "Bearer " + accessToken);
+        response.addHeader(jwtService.getRefreshHeader(), "Bearer " + refreshToken);
+
+        jwtService.sendAccessAndRefreshToken(response, accessToken, refreshToken);
+        jwtService.updateRefreshToken(oAuth2User.getEmail(), refreshToken);
+    }
+}
