e828be4b073795697f621709b8ac9f2332623d90
sangeun <olsang1203@gmail.com>
2024-08-06 11:55:30 +0900

    feat : 장소 리뷰 목록 조회, 유저 리뷰 목록 조회, 장소별 리뷰 요약 정보 조회 API 구현

--- src/main/java/com/example/locavel/converter/ReviewConverter.java
+++ src/main/java/com/example/locavel/converter/ReviewConverter.java
@@ -5,11 +5,20 @@ import com.example.locavel.domain.ReviewImg;
 import com.example.locavel.domain.Reviews;
 import com.example.locavel.domain.User;
 import com.example.locavel.domain.enums.Traveler;
+import com.example.locavel.repository.ReviewImgRepository;
 import com.example.locavel.web.dto.ReviewDTO.ReviewRequestDTO;
 import com.example.locavel.web.dto.ReviewDTO.ReviewResponseDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
+@RequiredArgsConstructor
+@Component
 public class ReviewConverter {
     public static ReviewResponseDTO.ReviewResultDTO toReviewResultDTO(Reviews review) {
         return ReviewResponseDTO.ReviewResultDTO.builder()
@@ -37,5 +46,93 @@ public class ReviewConverter {
                 .reviewId(review.getId())
                 .updatedAt(LocalDateTime.now())
                 .build();
+    }
+    public static ReviewResponseDTO.ReviewPreviewDTO toReviewPreviewDTO(
+            Reviews review, List<String> reviewImgList) {
+        User user = review.getUser();
+        return ReviewResponseDTO.ReviewPreviewDTO.builder()
+                .reviewId(review.getId())
+                .reviewerImg(user.getProfileImage())
+                .reviewerName(user.getNickname())
+                .traveler(Traveler.of(review.getPlace(),user).getViewName())
+                .reviewImgList(reviewImgList)
+                .Rating(review.getRating())
+                .comment(review.getComment())
+                .createdAt(review.getCreated_at())
+                .updatedAt(review.getUpdated_at())
+                .build();
+    }
+    public ReviewResponseDTO.ReviewPreviewListDTO toReviewPreviewListDTO(Page<Reviews> reviewList) {
+        List<ReviewResponseDTO.ReviewPreviewDTO> reviewPreViewDTOList = reviewList.stream()
+                .map(review -> {
+                    List<String> reviewImgList = getAllReviewImgUrls(review);
+                    return toReviewPreviewDTO(review,reviewImgList);
+                }).collect(Collectors.toList());
+        return ReviewResponseDTO.ReviewPreviewListDTO.builder()
+                .reviewList(reviewPreViewDTOList)
+                .listSize(reviewList.getSize())
+                .isFirst(reviewList.isFirst())
+                .isLast(reviewList.isLast())
+                .totalElements(reviewList.getTotalElements())
+                .totalPage(reviewList.getTotalPages())
+                .build();
+    }
+
+    public static ReviewResponseDTO.MyReviewDTO toMyReviewDTO(
+            Reviews review, List<String> reviewImgList) {
+        Places place = review.getPlace();
+        return ReviewResponseDTO.MyReviewDTO.builder()
+                .reviewId(review.getId())
+                .reviewImgList(reviewImgList)
+                .address(place.getAddress())
+                .placeName(place.getName())
+                .createdAt(review.getCreated_at())
+                .updatedAt(review.getUpdated_at())
+                .comment(review.getComment())
+                .Rating(review.getRating())
+                .build();
+    }
+
+    public ReviewResponseDTO.MyReviewListDTO toMyReviewListDTO(Page<Reviews> reviewList) {
+        List<ReviewResponseDTO.MyReviewDTO> myReviewDTOList = reviewList.stream()
+                .map(review ->{
+                    List<String> reviewImgList = getAllReviewImgUrls(review);
+                    return toMyReviewDTO(review,reviewImgList);
+                }).collect(Collectors.toList());
+        return ReviewResponseDTO.MyReviewListDTO.builder()
+                .isFirst(reviewList.isFirst())
+                .isLast(reviewList.isLast())
+                .listSize(reviewList.getSize())
+                .reviewList(myReviewDTOList)
+                .totalElements(reviewList.getTotalElements())
+                .totalPage(reviewList.getTotalPages())
+                .build();
+    }
+
+    public static ReviewResponseDTO.placeReviewSummaryDTO toPlaceReviewSummaryDTO(
+            Float totalRating,
+            Long totalReviewCount,
+            Float generalRating,
+            Long generalReviewCount,
+            Float travelerRating,
+            Long travelerReviewCount) {
+        return ReviewResponseDTO.placeReviewSummaryDTO.builder()
+                .totalRating(totalRating)
+                .totalReviewCount(totalReviewCount)
+                .generalRating(generalRating)
+                .generalReviewCount(generalReviewCount)
+                .travelerRating(travelerRating)
+                .travelerReviewCount(travelerReviewCount)
+                .build();
+    }
+
+    private final ReviewImgRepository reviewImgRepository;
+    public List<String> getAllReviewImgUrls(Reviews review) {
+        List<ReviewImg> reviewImgList = reviewImgRepository.findAllByReviews(review);
+        List<String> reviewImgUrlList = new ArrayList<>();
+        for(ReviewImg reviewImg : reviewImgList) {
+            reviewImgUrlList.add(reviewImg.getImgUrl());
+        }
+        return reviewImgUrlList;
     }
 }
--- src/main/java/com/example/locavel/web/dto/ReviewDTO/ReviewResponseDTO.java
+++ src/main/java/com/example/locavel/web/dto/ReviewDTO/ReviewResponseDTO.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 public class ReviewResponseDTO {
     @Builder
@@ -23,5 +24,74 @@ public class ReviewResponseDTO {
     public static class ReviewUpdateResultDTO {
         Long reviewId;
         LocalDateTime updatedAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreviewListDTO {
+        List<ReviewPreviewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class ReviewPreviewDTO {
+        Long reviewId;
+        String traveler;
+        String reviewerName;
+        String reviewerImg;
+        List<String> reviewImgList;
+        String comment;
+        Float Rating;
+        LocalDateTime createdAt;
+        LocalDateTime updatedAt;
+    }
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyReviewListDTO {
+        List<MyReviewDTO> reviewList;
+        Integer listSize;
+        Integer totalPage;
+        Long totalElements;
+        Boolean isFirst;
+        Boolean isLast;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class MyReviewDTO {
+        Long reviewId;
+        String placeName;
+        String address;
+        List<String> reviewImgList;
+        String comment;
+        Float Rating;
+        LocalDateTime createdAt;
+        LocalDateTime updatedAt;
+    }
+
+    @Builder
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class placeReviewSummaryDTO {
+        Float totalRating;
+        Long totalReviewCount;
+        Float generalRating;
+        Long generalReviewCount;
+        Float travelerRating;
+        Long travelerReviewCount;
     }
 }
