2b98c6305448ea84a39aee1d1ad7721c44f0b6b0
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-08-07 11:31:06 +0900

    Fix: Component 어노테이션 추가

--- src/main/java/com/example/locavel/converter/UserConverter.java
+++ src/main/java/com/example/locavel/converter/UserConverter.java
@@ -2,12 +2,29 @@ package com.example.locavel.converter;
 
 import com.example.locavel.domain.User;
 import com.example.locavel.web.dto.UserDTO.UserResponseDto;
+import org.springframework.stereotype.Component;
 
+@Component
 public class UserConverter {
 
     public static UserResponseDto.UpdateUserProfileResultDTO updateUserProfileResultDTO(User user){
         return UserResponseDto.UpdateUserProfileResultDTO.builder()
                 .profileImage(user.getProfileImage())
+                .build();
+    }
+
+    public UserResponseDto.getUserDTO toGetUserResultDTO(User user){
+        return UserResponseDto.getUserDTO.builder()
+                .user_id(user.getId())
+                .email(user.getEmail())
+                .introduce(user.getIntroduce())
+                .profileImage(user.getProfileImage())
+                .username(user.getUsername())
+                .nickname(user.getNickname())
+                .socialType(user.getSocialType())
+                .grade(user.getGrade())
+                .role(user.getRole())
+                .created_at(user.getCreated_at())
                 .build();
     }
 }
