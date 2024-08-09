7b0beb2d557d5bf5c54848345bd2cdaf97168db7
[J-neat] <[junghyo0258@catholic.ac.kr]>
2024-08-09 15:03:24 +0900

    Feat: TermAgreementRepository 생성

---
+++ src/main/java/com/example/locavel/repository/TermAgreementRepository.java
@@ -0,0 +1,7 @@
+package com.example.locavel.repository;
+
+import com.example.locavel.domain.mapping.TermAgreement;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TermAgreementRepository extends JpaRepository<TermAgreement, Long> {
+}
